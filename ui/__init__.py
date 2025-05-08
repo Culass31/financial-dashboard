e87f18cae5b3afefa2a4b2929fba5360fd676ed7
Culass31 <luc.nazarian@gmail.com>
2025-05-08 22:20:54 +0200

    Add files via upload

---
+++ ui/__init__.py
@@ -0,0 +1,21 @@
+"""
+Package contenant tous les composants UI de l'application
+"""
+
+from .sidebar import render_sidebar
+from .technical_analysis import render_technical_analysis_tab
+from .fundamental_analysis import render_fundamental_analysis_tab
+from .news import render_news_tab
+from .screener import render_screener_tab
+from .portfolio import render_portfolio_tab
+from .styles import apply_custom_styles
+
+__all__ = [
+    'render_sidebar',
+    'render_technical_analysis_tab',
+    'render_fundamental_analysis_tab',
+    'render_news_tab',
+    'render_screener_tab',
+    'render_portfolio_tab',
+    'apply_custom_styles'
+]
