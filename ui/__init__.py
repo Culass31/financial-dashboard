868705f401e486dabbd5428a9b281d37c6a493c0
Culass31 <luc.nazarian@gmail.com>
2025-05-09 00:20:01 +0200

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -8,7 +8,7 @@ from .fundamental_analysis import render_fundamental_analysis_tab
 from .news import render_news_tab
 from .screener import render_screener_tab
 from .portfolio import render_portfolio_tab
-from utils/ui_styles import apply_custom_styles
+from utils.ui_styles import apply_custom_styles
 
 __all__ = [
     'render_sidebar',
